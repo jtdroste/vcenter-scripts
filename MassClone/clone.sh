1831e63852e64568b5fb1770ac9819ee3d6a0efd
James Droste <james@droste.im>
2017-04-23 23:30:25 -0400

    updates to actually work

--- MassClone/clone.sh
+++ MassClone/clone.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+if [ "$#" -ne 2 ]; then
+	echo "Usage: $0 [input-list] [team-id]"
+	exit 1
+fi
 
 INPUT=$1
+TEAM=$2
+
+if [ ! -f "$INPUT" ]; then
+	echo "$INPUT file not found"
+	exit 99
+fi
+
+USERNAME=""
+PASSWORD=""
+
 OLDIFS=$IFS
 IFS=,
 
-[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-
-while read template datastore folder prefix
-do
-	for i in `seq 1 12`; do
-		python2.7 ../scripts/clone_vm.py --host cdr-vcenter1.cse.buffalo.edu -u ad\\UBIT -p UBITPW --datastore-name $datastore --template $template --vm-folder $folder --no-power-on --cluster MAIN  --vm-name "$prefix$i";
-	done
+while read template datastore folder vmprefix; do
+	python2.7 ../scripts/clone_vm.py --host cdr-vcenter1.cse.buffalo.edu -u $USERNAME -p $PASSWORD --datastore-name $datastore --template $template --vm-folder $folder --no-power-on --cluster MAIN  --vm-name "${vmprefix}${TEAM}";
 done < $INPUT
 
 IFS=$OLDIFS
--- README.md
+++ README.md
@@ -6,13 +6,17 @@ pip install -r requirements.txt
 ```
 
 ## MassClone
-MassClone was a script built for UBNetDef's competition - Lockdown.  It takes in an input file, which has the following information:
+MassClone was a script built for UBNetDef's competition - Lockdown.  It takes in an input file and a team id.  The input file must have the following information:
 
 ```
 VM_Template_Name,datastore_to_store,folder_to_save_to,prefix_of_vm_
 ```
 
-It will utilize the script located in `scripts/clone_vm.py`.
+It will utilize the script located in `scripts/clone_vm.py`.  To mass clone multiple times, wrap it into a loop.  Example:
+
+```
+for i in `seq 1 11`; do ./clone.sh list $i; done;
+```
 
 ## Samples
 This folder contains a bunch of community samples from vcenter. They *should* work.
